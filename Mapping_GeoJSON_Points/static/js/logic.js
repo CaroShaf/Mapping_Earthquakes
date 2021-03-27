d0ebf86eafc355967c099876a7ec31df8d7179b0
Carolyn Shaffer <caroshaf@charter.net>
2021-03-27 14:22:33 -0500

    airport data json file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -26,12 +26,13 @@ let sanFranAirport =
 // Grabbing our GeoJSON data.
 L.geoJson(sanFranAirport, {
     onEachFeature: function(feature, layer) {
-      layer.bindPopup();
+      console.log(layer);
+      layer.bindPopup("Airport code:" + feature.properties.faa + "<hr>" + "Airport name:" + feature.properties.name);
      }
-});
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
